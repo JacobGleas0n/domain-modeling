c02f22e20073675bfbe4deae4243555b2eb25ab4
JacobGleas0n <jacob.gleason@kellogg.northwestern.edu>
2022-01-18 17:16:34 +0000

    class exercise commit

--- crm/crm.sql
+++ crm/crm.sql
@@ -3,3 +3,52 @@
 -- e.g. DROP TABLE IF EXISTS ______;
 
 -- CREATE TABLES
+
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS contacts;
+DROP TABLE IF EXISTS interactions;
+DROP TABLE IF EXISTS companies;
+DROP TABLE IF EXISTS industries;
+DROP TABLE IF EXISTS industry_memberships;
+
+CREATE TABLE users (
+ id INTEGER PRIMARY KEY AUTOINCREMENT,
+ first_name TEXT, 
+ last_name TEXT,
+ email TEXT
+ );
+
+ CREATE TABLE contacts (
+ id INTEGER PRIMARY KEY AUTOINCREMENT, 
+ name TEXT,
+ email TEXT,
+ phone_number TEXT,
+ title TEXT,
+ company_id INTEGER
+ );
+
+ CREATE TABLE interactions (
+ id INTEGER PRIMARY KEY AUTOINCREMENT,
+ interaction_type TEXT,
+ date TEXT,
+ time TEXT,
+ notes TEXT,
+ contact_ID INTEGER,
+ user_ID INTEGER
+ );
+
+ CREATE TABLE companies (
+ id INTEGER PRIMARY KEY AUTOINCREMENT,
+ name TEXT    
+ );
+
+ CREATE TABLE industries (
+ id INTEGER PRIMARY KEY AUTOINCREMENT,
+ name TEXT
+ );
+
+ CREATE TABLE industry_memberships (
+ id INTEGER PRIMARY KEY AUTOINCREMENT,
+ company_id INTEGER,
+ industry_id INTEGER
+ );
--- tacostagram/tacostagram.sql
+++ tacostagram/tacostagram.sql
@@ -1,1 +1,39 @@
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS posts;
+DROP TABLE IF EXISTS likes;
+DROP TABLE IF EXISTS comments;
+DROP TABLE IF EXISTS followers;
 
+CREATE TABLE users ( 
+ id INTEGER PRIMARY KEY AUTOINCREMENT,
+ screen_name TEXT,
+ first_name TEXT,
+ last_name TEXT,
+ location TEXT
+);
+
+CREATE TABLE posts ( 
+ id INTEGER PRIMARY KEY AUTOINCREMENT,
+ time TEXT,
+ photo TEXT,
+ user_id INTEGER
+ );
+
+CREATE TABLE likes (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+post_id INTEGER,
+user_id INTEGER
+);
+
+CREATE TABLE comments (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+post_id INTEGER,
+user_id INTEGER,
+comment TEXT
+);
+
+CREATE TABLE followers (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+followed_user_id INTEGER,
+follower_user_id INTEGER
+);
